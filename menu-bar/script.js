cb83561a2bd7e98aeb9c38e4433f065c1699719e
saideepak-02 <109219471+saideepak-02@users.noreply.github.com>
2023-11-06 21:30:41 +0530

    Update script.js

--- menu-bar/script.js
+++ menu-bar/script.js
@@ -1,4 +1,12 @@
+let show=false;
 function pop(){
     let ref=document.getElementById("list");
+    if(show==false){
     ref.style.display="block";
+    show="true";
+    }
+    else
+    {ref.style.display="none";
+    show=false;
+    }
 }
